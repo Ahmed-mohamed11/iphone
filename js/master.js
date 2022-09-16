c7870ca0054dfa7ff5eede3c29e2fc98dcb8ca30
Ahmedqr1 <100643038+Ahmedqr1@users.noreply.github.com>
2022-09-16 05:03:52 +0200

    first commit

---
+++ js/master.js
@@ -0,0 +1,13 @@
+let img = document.querySelector('.img');
+let content = document.querySelector('.content');
+
+
+function phones(src) {
+    img.src = src;
+}
+
+console.log("ahmed");
+
+function colors(color) {
+    content.style.background = color;
+}
